bd38ad662c20c8212f4eb1c0222dd8a55980e901
VoidCodes <harodtai321@gmail.com>
2021-08-18 13:54:55 +0800

    Filtered countries with 0 visitors during

--- DA_asia-group.py
+++ DA_asia-group.py
@@ -1,1 +1,13 @@
-print("test5401")
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+class Statistics:
+    countries = pd.read_excel('Project_File')
+    countries['Brunei Darussalam'] = countries['Brunei Darussalam'].replace(['na'], 0)
+    countries['Indonesia'] = countries['Indonesia'].replace(['na'], 0)
+    countries['Malaysia'] = countries['Malaysia'].replace(['na'], 0)
+    countries['Philippines'] = countries['Philippines'].replace(['na'], 0)
+    countries['Thailand'] = countries['Thailand'].replace(['na'], 0)
+    countries['Viet Nam'] = countries['Viet Nam'].replace(['na'], 0)
+    countries['Myanmar'] = countries['Myanmar'].replace(['na'], 0)
