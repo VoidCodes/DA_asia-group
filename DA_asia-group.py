162d698ee9f06c7d58f8835c2f29e42217255035
VoidCodes <harodtai321@gmail.com>
2021-08-19 11:38:28 +0800

    Refactored the code so that it generates a graph

--- DA_asia-group.py
+++ DA_asia-group.py
@@ -1,103 +1,72 @@
-import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-from pandas.core.frame import DataFrame as df
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Statistics:
 
     def __init__(self):
-        self.countries = pd.read_excel('Project_File.xlsx')
-    
+        self.countries = pd.read_excel(r"Project_File.xlsx")
+
     def generate_Top3(self):
-        self.countries['Brunei Darussalam'] = self.countries['Brunei Darussalam'].replace(['na'], 0)
-        self.countries['Indonesia'] = self.countries['Indonesia'].replace(['na'], 0)
-        self.countries['Malaysia'] = self.countries['Malaysia'].replace(['na'], 0)
-        self.countries['Philippines'] = self.countries['Philippines'].replace(['na'], 0)
-        self.countries['Thailand'] = self.countries['Thailand'].replace(['na'], 0)
-        self.countries['Viet Nam'] = self.countries['Viet Nam'].replace(['na'], 0)
-        self.countries['Myanmar'] = self.countries['Myanmar'].replace(['na'], 0)
+        countryRegion = self.countries
+        countryRegion = countryRegion.replace("na", 0)
+        AsiaPacific = countryRegion.iloc[:,lambda df:[0, 8, 9, 10, 11, 12]]
+        AsiaPacific = AsiaPacific.set_index('Unnamed: 0')
+        countryYear = AsiaPacific.loc['1978 Jan':'1987 Dec']
+        print (countryYear)
+        Sums = AsiaPacific.sum(axis=0)
+        Sums = Sums.sort_values(ascending=False)
+        print (Sums)
+        top1 = Sums.index[0]
+        top2 = Sums.index[1]
+        top3 = Sums.index[2]
+        print("The top 3 countries with the most visitors are", top1 , top2 , top3)
 
-        bruneiTotalVisitors = self.countries['Brunei Darussalam'].sum()
-        print(bruneiTotalVisitors, " - Brunei")
 
-        indonesiaTotalVisitors = self.countries['Indonesia'].sum()
-        indonesiaMeanVisitors = self.countries['Indonesia'].mean()
-        indonesiaMedianVisitors = self.countries['Indonesia'].median()
-        #print(indonesiaMedianVisitors)
-        print(indonesiaTotalVisitors, " - Indonesia")
+        vis1 = Sums[top1]
+        vis2 = Sums[top2]
+        vis3 = Sums[top3]
+        print("The Amount of visitors from the top 3 countries are", vis1 , vis2 , vis3, "respectively")
 
-        malaysiaTotalVisitors = self.countries['Malaysia'].sum()
-        print(malaysiaTotalVisitors, " - M'sia")
 
-        philippinesTotalVisitors = self.countries['Philippines'].sum()
-        print(philippinesTotalVisitors, " - Philipines")
+        means = AsiaPacific.mean(axis=0)
+        means = means.sort_values(ascending=False)
+        print (means)
+        mean1 = round(means[0],2)
+        mean2 = round(means[1],2)
+        mean3 = round(means[2],2)
+        print("The Average visitors from the top 3 countries are", mean1 , mean2 , mean3, "respectively")
 
-        thailandTotalVisitors = self.countries['Thailand'].sum()
-        print(thailandTotalVisitors, " - Thailand")
 
-        vietnamTotalVisitors = self.countries['Viet Nam'].sum()
-        print(vietnamTotalVisitors, " - Vietnam")
+        medians = AsiaPacific.median(axis=0)
+        medians = medians.sort_values(ascending=False)
+        print (medians)
+        med1 = medians[0]
+        med2 = medians[1]
+        med3 = medians[2]
+        print("The Median visitors from the top 3 countries are", med1 , med2 , med3, "respectively")
 
-        myanmarTotalVisitors = self.countries['Myanmar'].sum()
-        print(myanmarTotalVisitors, " - Myanmar")
+    def generate_linegraph(self):
+        df = self.countries
+        df = df.replace("na", 0)
+        df = df.iloc[:,lambda df:[0, 8, 9, 10, 11, 12]]
+        df2 = df.set_index('Unnamed: 0')
+        df2 = df2.loc['1978 Jan':'1987 Dec']
 
-        hongkongTotalVisitors = self.countries['Hong Kong'].sum()
-        print(hongkongTotalVisitors, " - Hk")
-
-        chinaTotalVisitors = self.countries['China'].sum()
-        chinaMeanVisitors = self.countries['China'].mean()
-        chinaMedianVisitors = self.countries['China'].median()
-        #print(chinaMedianVisitors)
-        print(chinaTotalVisitors, " - China")
-
-        taiwanTotalVisitors = self.countries['Taiwan'].sum()
-        print(taiwanTotalVisitors, " - Taiwan")
-
-        koreaTotalVisitors = self.countries['Korea, Republic Of'].sum()
-        print(koreaTotalVisitors, " - Korea")
-
-        japanTotalVisitors = self.countries['Japan'].sum()
-        japanMeanVisitors = self.countries['Japan'].mean()
-        japanMedianVisitors = self.countries['Japan'].median()
-        #print(japanMedianVisitors)
-        print(japanTotalVisitors, " - Jap")
-
-        print("The top 3 countries with the most visitors are Indonesia, Japan, China")
-        print("The total numbers of visitors from the top 3 individual countries are " + str(indonesiaTotalVisitors) + ", " + str(japanTotalVisitors) + " and " + str(chinaTotalVisitors))
-        print("The mean value of visitors from the top 3 individual countries countries are " + str(round(indonesiaMeanVisitors, 2)) + ", " + str(round(japanMeanVisitors, 2)) + " and " + str(round(chinaMeanVisitors, 2)))
-        print("The median value of visitors from the top 3 individual countries are " + str(indonesiaMedianVisitors) + ", " + str(japanMedianVisitors) + " and " + str(chinaMedianVisitors))
-
-    def generateLineChart(self):            
-        #print(countries)
-        years = self.countries[self.countries.columns[0]]
-        #print(years)
-
-        year = years.str.split(" ", n=1, expand=True)
-        #print(year)
-
-        countries = self.countries.assign(Year=year[0])
-        countries = countries.assign(Month=year[1])
-
-        yearstats = countries.groupby("Year").sum().reset_index()
-        print(yearstats)
-        #print(years[years.str.contains("Jan")])
-        plt.title("Stats")
-        plt.plot(yearstats["Japan"])
-        plt.xlabel("Year")
-        plt.ylabel("Vistors")
-
-        #plt.legend()
+        date_sum = df2.sum(axis=1)
+        months = np.zeros((12,), dtype=int).tolist()
+        month_names = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+        for j in range(0,12):
+            for i in np.arange(j,120,12):
+                months[j] += date_sum.iat[i]
+        print (months)
+        plt.plot(month_names,months)
         plt.show()
 
-    def generateBarGraph(self):
-        yearStats = self.countries.groupby("Year").sum().reset_index()
+    def generate_bargraph(self):
+        # Generate bar graph from the top 3 countries with visitor value
+        print("hi")
 
-        plt.bar(yearStats["Year"], yearStats["Japan"])
-        plt.xlabel("Year")
-        plt.ylabel("Country")
-        plt.show()
-    
 stats = Statistics()
 stats.generate_Top3()
-stats.generateLineChart()
-#stats.generateBarGraph()
+stats.generate_linegraph()
